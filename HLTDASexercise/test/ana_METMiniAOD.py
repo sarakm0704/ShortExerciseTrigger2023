e2c30e5d1b112cd36b59b6df34d0b101cc8cb4cd
olivito <dominick.olivito@gmail.com>
2017-01-04 15:09:01 +0100

    config for MET eff analyzer

---
+++ HLTDASexercise/test/ana_METMiniAOD.py
@@ -0,0 +1,54 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLTANALYZER')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+   fileNames = cms.untracked.vstring('/store/data/Run2016G/SingleElectron/MINIAOD/23Sep2016-v1/100000/004A7893-A990-E611-B29F-002590E7DE36.root'),
+#   fileNames = cms.untracked.vstring('/store/user/hats/Trigger/2016/skim_JetHT_pfht475_15k.root'), # file on EOS at LPC
+)
+
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.20 $'),
+    annotation = cms.untracked.string('RelVal nevts:100'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.TFileService = cms.Service("TFileService",
+                                       fileName = cms.string('histos_METTrigAnalyzer.root')
+                                   )
+
+### analyzer configuration
+
+process.metTrigAnalyzerMiniAOD = cms.EDAnalyzer("METTrigAnalyzerMiniAOD")
+process.metTrigAnalyzerMiniAOD.refTriggerName = cms.untracked.string("HLT_Ele27_eta2p1_WPTight_Gsf_v7")
+process.metTrigAnalyzerMiniAOD.sigTriggerName = cms.untracked.string("HLT_PFMET170_HBHECleaned_v6")
+process.metTrigAnalyzerMiniAOD.verbose = cms.untracked.bool(False)
+
+process.GlobalTag.globaltag = "80X_dataRun2_2016SeptRepro_v3"
+
+# Path and EndPath definitions
+process.HLTanalyzers = cms.Path(process.metTrigAnalyzerMiniAOD)
