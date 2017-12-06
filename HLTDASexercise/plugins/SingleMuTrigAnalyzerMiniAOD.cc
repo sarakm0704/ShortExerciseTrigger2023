c92b014b44a64f6b3470131c719bd691762cd115
Javier Duarte <jduarte1@fnal.gov>
2017-12-06 15:34:14 -0600

    update for 2018

--- HLTDASexercise/plugins/SingleMuTrigAnalyzerMiniAOD.cc
+++ HLTDASexercise/plugins/SingleMuTrigAnalyzerMiniAOD.cc
@@ -8,7 +8,7 @@
 
 #include "FWCore/Common/interface/TriggerNames.h"
 #include "FWCore/Common/interface/TriggerResultsByName.h"
-#include "HLTDAS2017/HLTDASexercise/interface/SingleMuTrigAnalyzerMiniAOD.h"
+#include "HLTDAS2018/HLTDASexercise/interface/SingleMuTrigAnalyzerMiniAOD.h"
 
 #include "DataFormats/HLTReco/interface/TriggerObject.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
