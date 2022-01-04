9490f3d12761c1135a72a44b5794fd2efc2e827d
gparida <65186277+gparida@users.noreply.github.com>
2022-01-04 11:55:45 -0600

    Update SealModule.cc

--- ShortExerciseTrigger/plugins/SealModule.cc
+++ ShortExerciseTrigger/plugins/SealModule.cc
@@ -1,8 +1,8 @@
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "ShortExerciseTrigger2021/ShortExerciseTrigger/interface/SingleMuTrigAnalyzerMiniAOD.h"
-#include "ShortExerciseTrigger2021/ShortExerciseTrigger/interface/METTrigAnalyzerMiniAOD.h"
+#include "ShortExerciseTrigger2022/ShortExerciseTrigger/interface/SingleMuTrigAnalyzerMiniAOD.h"
+#include "ShortExerciseTrigger2022/ShortExerciseTrigger/interface/METTrigAnalyzerMiniAOD.h"
 
 DEFINE_FWK_MODULE(SingleMuTrigAnalyzerMiniAOD);
 DEFINE_FWK_MODULE(METTrigAnalyzerMiniAOD);
